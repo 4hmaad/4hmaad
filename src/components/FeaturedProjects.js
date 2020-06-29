b61c5d46b564fbffa67debb8487bc16eecf46fe2
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-06-29 21:18:46 +0500

    add featured projects section and also add projects data to data folder

---
+++ src/components/FeaturedProjects.js
@@ -0,0 +1,60 @@
+import React from "react"
+import styled from "styled-components"
+import { useStaticQuery, graphql } from "gatsby"
+
+/* components */
+import Project from "./Project"
+
+/* Typography */
+import H from "./typography/Headings"
+
+const ProjectsStyles = styled.section`
+  padding: var(--section-padding);
+`
+
+export const FeaturedProjects = () => {
+  const {
+    allMdx: { nodes: projects },
+  } = useStaticQuery(graphql`
+    query GET_PROJECTS_QUERY {
+      allMdx(
+        filter: {
+          fileAbsolutePath: { regex: "/projects/" }
+          frontmatter: { visible: { eq: true } }
+        }
+      ) {
+        nodes {
+          frontmatter {
+            title
+            description
+            stack
+            site
+            repo
+            image {
+              childImageSharp {
+                fluid(maxWidth: 1000, quality: 100) {
+                  base64
+                  aspectRatio
+                  src
+                  srcSet
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <ProjectsStyles>
+      <H>Selected Projects</H>
+
+      {projects.map(({ frontmatter }, idx) => (
+        <Project key={idx} project={frontmatter} />
+      ))}
+    </ProjectsStyles>
+  )
+}
+
+export default FeaturedProjects
---
+++ src/components/Project.js
@@ -0,0 +1,84 @@
+import React from "react"
+import styled from "styled-components"
+import Img from "gatsby-image"
+
+/* components */
+import CustomButton from "./CustomButton"
+
+/* typography */
+import H from "./typography/Headings"
+import P from "./typography/Text"
+import A from "./typography/Link"
+
+const ProjectStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(40rem, 1fr));
+  grid-gap: 4rem;
+  align-items: center;
+  margin-bottom: 10rem;
+`
+
+const ProjectImageContainer = styled.a`
+  height: 40rem;
+  overflow: hidden;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 40px -15px;
+  transition: 0.3s ease;
+
+  & > img,
+  picture {
+    object-position: top !important;
+    object-fit: cover !important;
+    width: 100% !important;
+  }
+
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 0px 60px -15px;
+  }
+`
+
+const ProjectLinksContainer = styled.div`
+  margin-top: 4rem;
+
+  & > a:last-child {
+    margin-left: 2rem;
+  }
+`
+
+const ListUi = styled.ul`
+  list-style-type: none;
+`
+
+const Project = props => {
+  const { title, description, image, repo, site, stack } = props.project
+
+  return (
+    <ProjectStyles>
+      <div>
+        <H as="h3">{title}</H>
+
+        <P>{description}</P>
+
+        <H as="h5">Tools Used</H>
+
+        <ListUi>
+          {stack.map((tool, idx) => (
+            <li key={idx}>{tool}</li>
+          ))}
+        </ListUi>
+
+        <ProjectLinksContainer>
+          <CustomButton small href={site}>
+            Visit Site
+          </CustomButton>
+          <A href={repo}>Source Code</A>
+        </ProjectLinksContainer>
+      </div>
+
+      <ProjectImageContainer href="#">
+        <Img fluid={image.childImageSharp.fluid} alt={title} />
+      </ProjectImageContainer>
+    </ProjectStyles>
+  )
+}
+
+export default Project

d883717644ac603526ee4a04e4aa3892483f0404
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-06-30 14:42:50 +0500

    add icons as react components in icons folder

---
+++ src/components/icons/index.js
@@ -0,0 +1,8 @@
+import MoonIcon from "./moon"
+import SunIcon from "./sun"
+import GithubIcon from "./github"
+import LinkedInIcon from "./linkedin"
+import ArrowIcon from "./arrow"
+import Logo from "./logo"
+
+export { MoonIcon, SunIcon, GithubIcon, LinkedInIcon, ArrowIcon, Logo }

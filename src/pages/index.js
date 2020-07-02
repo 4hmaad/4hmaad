0526e968e40b758043d69c16f264d7a5dcdbddec
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-07-02 09:01:43 +0500

    add the new nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ import Footer from "./../components/Footer"
 export default function Home() {
   return (
     <Layout>
-      <Nav showLinks />
+      <Nav />
       <Hero />
       <FeaturedProjects />
       <About />

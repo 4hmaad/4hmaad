95fa8d1a0a065ac083ca1099738a2d955495b0bb
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-07-03 21:58:09 +0500

    improve gatsby-node code

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,61 +1,70 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const path = require(`path`)
 
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
-  const PostTemplate = path.resolve("src/templates/PostTemplate.js")
-  return graphql(`
-    {
-      allMdx(
-        sort: { fields: [frontmatter___date], order: DESC }
-        filter: {
-          frontmatter: { published: { eq: true } }
-          fileAbsolutePath: { regex: "/posts/" }
-        }
-      ) {
-        nodes {
-          fields {
-            slug
+async function convertMdxToPosts({ graphql, actions }) {
+  const PostTemplate = path.resolve("./src/templates/PostTemplate")
+
+  const { errors, data } = await graphql(
+    `
+      {
+        allMdx(
+          sort: { fields: [frontmatter___date], order: DESC }
+          filter: {
+            frontmatter: { published: { eq: true } }
+            fileAbsolutePath: { regex: "/posts/" }
           }
-          frontmatter {
-            title
+        ) {
+          nodes {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
           }
         }
       }
-    }
-  `).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-    const posts = result.data.allMdx.nodes
-    // create page for each mdx file
-    posts.forEach((post, index) => {
-      // assign null if it's the last published post, remember it's for descending order.
-      const previous = index === posts.length - 1 ? null : posts[index + 1]
-      // assign null if its the first published post.
-      const next = index === 0 ? null : posts[index - 1]
+    `
+  )
 
-      createPage({
-        path: post.fields.slug,
-        component: PostTemplate,
-        context: {
-          slug: post.fields.slug,
-          next,
-          previous,
-        },
-      })
+  if (errors) {
+    throw new Error(errors)
+  }
+
+  const posts = data.allMdx.nodes
+
+  posts.forEach((post, idx) => {
+    const previous = idx === posts.length - 1 ? null : posts[idx + 1]
+    const next = idx === 0 ? null : posts[idx - 1]
+
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPost,
+      context: {
+        slug: post.fields.slug,
+        collection: "post",
+        previous,
+        next,
+        pathPrefix: "",
+      },
     })
   })
+}
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+  convertMdxToPosts({ graphql, createPage })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
   if (node.internal.type === `Mdx`) {
-    const value = createFilePath({ node, getNode })
+    const generatedSlug = createFilePath({ node, getNode })
+
     createNodeField({
       name: `slug`,
       node,
-      value,
+      value: generatedSlug,
     })
   }
 }

d44a5a6ab0ddf06d105d19a36caafe77fc86f434
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-07-05 16:01:47 +0500

    add Code component to display code inside blog

---
+++ src/components/Code.js
@@ -0,0 +1,56 @@
+import React from "react"
+import styled from "styled-components"
+import Highlight, { defaultProps } from "prism-react-renderer"
+import theme from "prism-react-renderer/themes/nightOwl"
+
+const Pre = styled.pre`
+  text-align: left;
+  margin: 2rem 0;
+  padding: 0.5rem;
+  overflow-x: auto;
+  border-radius: 3px;
+  & .token-line {
+    line-height: 1.6;
+  }
+  font-family: "Fira Code", Courier, monospace;
+`
+const LineNo = styled.span`
+  display: inline-block;
+  min-width: 3.2rem;
+  user-select: none;
+  opacity: 0.3;
+  margin-right: 0.5rem;
+`
+
+const Code = ({ codeString, language }) => {
+  console.log(codeString, language)
+  console.log(defaultProps)
+
+  return (
+    <Highlight
+      {...defaultProps}
+      code={codeString}
+      language={language}
+      theme={theme}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => {
+        console.log({ className, style, tokens, getLineProps, getTokenProps })
+
+        return (
+          <Pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                <LineNo>{i + 1}</LineNo>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </Pre>
+        )
+      }}
+    </Highlight>
+  )
+}
+
+export default Code
